c6bebf82aa53fa3ffefe16c558d0fe20abfa3586
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-07-17 23:04:46 -0400

    Reapply "Reapply "Reapply "Switch to https://github.com/HaxeFoundation/hxcpp/tree/1fd1f4910fdaf416443f84a48da5048ebf0bdfc3"""
    This reverts commit 78a784058b13a8c59697700d060bd79311506b0b.

--- src/hx/Debug.cpp
+++ src/hx/Debug.cpp
@@ -7,6 +7,7 @@
 #include <hx/Thread.h>
 #include <hx/Telemetry.h>
 #include <hx/Unordered.h>
+#include <hx/thread/Thread.hpp>
 #include <hx/OS.h>
 #include <mutex>
 
@@ -193,6 +194,10 @@ StackContext::StackContext()
    mIsUnwindingException = false;
    #endif
 
+   #ifdef HXCPP_FUTURE_GC
+   mFutureDirty = 0;
+   #endif
+
    #if HXCPP_TELEMETRY
    mTelemetry = tlmCreate(this);
    #endif
@@ -244,7 +249,7 @@ StackContext::~StackContext()
 void StackContext::onThreadAttach()
 {
    #ifdef HXCPP_STACK_IDS
-   mThreadId = __hxcpp_GetCurrentThreadNumber();
+    mThreadId = hx::thread::Thread_obj::id();
 
    {
        std::lock_guard<std::mutex> guard(sStackMapMutex);
